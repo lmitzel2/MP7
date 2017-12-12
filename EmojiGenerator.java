52297e88877aeeb3f7f3a938e0ae0b530a1d0582
Logan Mitzel <32113914+lmitzel2@users.noreply.github.com>
2017-12-11 18:03:37 -0600

    Update EmojiGenerator.java

--- EmojiGenerator.java
+++ EmojiGenerator.java
@@ -15,65 +15,65 @@ public class EmojiGenerator {
 		int userInt;
 		Scanner in = new Scanner(System.in);
 		try {
+		userInt = in.nextInt();
+		while (userInt < min || userInt > max) {
+			System.out.println("Please rate your mood from 1 to 10 (1 being low mood, and 10 being high mood).");
 			userInt = in.nextInt();
-			while (userInt < min || userInt > max) {
-				System.out.println("Please rate your mood from 1 to 10 (1 being low mood, and 10 being high mood).");
-				userInt = in.nextInt();
-			}
-			if (userInt == 1 || userInt == 2) {
-				System.out.println("          ");
-				System.out.println(" ##    ## ");
-				System.out.println(" ##    ## ");
-				System.out.println(" ##    ## ");
-				System.out.println("          ");
-				System.out.println("          ");
-				System.out.println("  ######  ");
-				System.out.println(" ######## ");
-				System.out.println("###    ###");
-				System.out.println("##      ##");
-			} else if (userInt == 3 || userInt == 4) {
-				System.out.println("           ");
-				System.out.println("  ###  ### ");
-				System.out.println("  ###  ### ");
-				System.out.println("  # #  # # ");
-				System.out.println("           ");
-				System.out.println("           ");
-				System.out.println("     ###   ");
-				System.out.println("   ####### ");
-				System.out.println(" ##      ##");
-			} else if (userInt == 5 || userInt == 6) {
-				System.out.println("          ");
-				System.out.println(" ##    ## ");
-				System.out.println(" ##    ## ");
-				System.out.println(" ##    ## ");
-				System.out.println("          ");
-				System.out.println("          ");
-				System.out.println("          ");
-				System.out.println("  ######  ");
-				System.out.println("          ");
-			} else if (userInt == 7 || userInt == 8) {
-				System.out.println("          ");
-				System.out.println(" ##    ## ");
-				System.out.println(" ##    ## ");
-				System.out.println(" ##    ## ");
-				System.out.println("          ");
-				System.out.println("          ");
-				System.out.println("##      ##");
-				System.out.println("###    ###");
-				System.out.println(" ######## ");
-				System.out.println("  ######  ");
-			} else {
-				System.out.println("          ");
-				System.out.println(" ##    ## ");
-				System.out.println(" ##    ## ");
-				System.out.println(" ##    ## ");
-				System.out.println("          ");
-				System.out.println("          ");
-				System.out.println("##########");
-				System.out.println("##      ##");
-				System.out.println(" ##    ## ");
-				System.out.println("  ######  ");
-			}
+		}
+		if (userInt == 1 || userInt == 2) {
+			System.out.println("          ");
+			System.out.println(" ##    ## ");
+			System.out.println(" ##    ## ");
+			System.out.println(" ##    ## ");
+			System.out.println("          ");
+			System.out.println("          ");
+			System.out.println("  ######  ");
+			System.out.println(" ######## ");
+			System.out.println("###    ###");
+			System.out.println("##      ##");
+		} else if (userInt == 3 || userInt == 4) {
+			System.out.println("           ");
+			System.out.println("  ###  ### ");
+			System.out.println("  ###  ### ");
+			System.out.println("  # #  # # ");
+			System.out.println("           ");
+			System.out.println("           ");
+			System.out.println("     ###   ");
+			System.out.println("   ####### ");
+			System.out.println(" ##      ##");
+		} else if (userInt == 5 || userInt == 6) {
+			System.out.println("          ");
+			System.out.println(" ##    ## ");
+			System.out.println(" ##    ## ");
+			System.out.println(" ##    ## ");
+			System.out.println("          ");
+			System.out.println("          ");
+			System.out.println("          ");
+			System.out.println("  ######  ");
+			System.out.println("          ");
+		} else if (userInt == 7 || userInt == 8) {
+			System.out.println("          ");
+			System.out.println(" ##    ## ");
+			System.out.println(" ##    ## ");
+			System.out.println(" ##    ## ");
+			System.out.println("          ");
+			System.out.println("          ");
+			System.out.println("##      ##");
+			System.out.println("###    ###");
+			System.out.println(" ######## ");
+			System.out.println("  ######  ");
+		} else {
+			System.out.println("          ");
+			System.out.println(" ##    ## ");
+			System.out.println(" ##    ## ");
+			System.out.println(" ##    ## ");
+			System.out.println("          ");
+			System.out.println("          ");
+			System.out.println("##########");
+			System.out.println("##      ##");
+			System.out.println(" ##    ## ");
+			System.out.println("  ######  ");
+		}
 		} catch (InputMismatchException exc) {
 			System.out.println("Incorrect input: an integer is required. Try again.");
 			giveMeEmoji();
